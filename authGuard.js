ef6e6ba0fc3e3491b91dcbae0214451056e2a04d
DalubwikaanNEU <ycherochristo.amancio@neu.edu.ph>
2026-07-22 17:28:12 +0800

    Update authGuard.js

--- authGuard.js
+++ authGuard.js
@@ -1,28 +1,47 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+
 import {
     getAuth,
-    onAuthStateChanged
+    onAuthStateChanged,
+    signOut
 } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDx5TR1iYZZsK4JqlvCmuR_0U6H1d3Mr80",
-    authDomain: "dalubwikaan--26-8e646.firebaseapp.com",
-    projectId: "dalubwikaan--26-8e646",
-    storageBucket: "dalubwikaan--26-8e646.firebasestorage.app",
-    messagingSenderId: "409516392020",
-    appId: "1:409516392020:web:87d462a5927449c69eb7c1"
+
+    apiKey: "...",
+
+    authDomain: "...",
+
+    projectId: "...",
+
+    storageBucket: "...",
+
+    messagingSenderId: "...",
+
+    appId: "..."
+
 };
 
 const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 
-onAuthStateChanged(auth, (user) => {
+onAuthStateChanged(auth, async (user) => {
 
-    if (user) {
-        alert("Logged in: " + user.email);
-    } else {
-        alert("No User");
-        window.location.href = "login.html";
+    if (!user) {
+
+        window.location.replace("login.html");
+        return;
+    }
+
+    if (user.email !== "admin@dalubwikaan.com") {
+
+        alert("🚫 Access Denied");
+
+        await signOut(auth);
+
+        window.location.replace("login.html");
+
     }
 
 });
