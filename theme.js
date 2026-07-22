4e313e7ef3f7e47bb1f12d34ced09dcddfc526b1
DalubwikaanNEU <ycherochristo.amancio@neu.edu.ph>
2026-07-22 18:26:12 +0800

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,41 +1,40 @@
-alert("THEME JS LOADED");
+console.log("THEME JS LOADED");
+
 const button = document.getElementById("themeToggle");
 
-// Load saved theme
-const savedTheme = localStorage.getItem("theme");
 
-if (savedTheme === "dark") {
+let savedTheme = localStorage.getItem("theme");
+
+
+if(savedTheme === "dark"){
 
     document.body.classList.add("dark");
 
-    if (button) {
+    button.innerHTML="☀️ Light Mode";
 
-        button.innerHTML = "☀️ Light Mode";
+}
+
+
+button.addEventListener("click",()=>{
+
+
+    document.body.classList.toggle("dark");
+
+
+    if(document.body.classList.contains("dark")){
+
+        localStorage.setItem("theme","dark");
+
+        button.innerHTML="☀️ Light Mode";
+
+
+    }else{
+
+        localStorage.setItem("theme","light");
+
+        button.innerHTML="🌙 Dark Mode";
 
     }
 
-}
 
-if (button) {
-
-    button.addEventListener("click", () => {
-
-        document.body.classList.toggle("dark");
-
-        if (document.body.classList.contains("dark")) {
-
-            localStorage.setItem("theme", "dark");
-
-            button.innerHTML = "☀️ Light Mode";
-
-        } else {
-
-            localStorage.setItem("theme", "light");
-
-            button.innerHTML = "🌙 Dark Mode";
-
-        }
-
-    });
-
-}
+});
